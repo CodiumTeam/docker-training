681ceb97bf680ebffaf452b1b22b62990480728e
Isidro Lopez <islomar@gmail.com>
2021-05-11 11:00:36 +0200

    Find users in the flask example

--- exercise-4/nginx-flask-mongo/flask/server.py
+++ exercise-4/nginx-flask-mongo/flask/server.py
@@ -14,8 +14,22 @@ client = MongoClient("mongo:27017")
 def todo():
     try:
         app.logger.info('Processing request...')
+
+        users_db = client.users
+        app.logger.info('Users db: {}'.format(users_db))
+        test_collection = users_db.test_collection
+        collection_names = users_db.list_collection_names()
+        app.logger.info('Collection names: {}'.format(collection_names))
+
+        test_collection.insert_one({"name": "jonas"})
+        one_user = test_collection.find_one()
+        app.logger.info('One user: {}'.format(one_user))
+        all_users = test_collection.find()
+        app.logger.info('All users: {}'.format(all_users))
+
         client.admin.command('ismaster')
-    except:
+    except Exception as ex:
+        app.logger.error(ex)
         return "Server not available"
     return "Hello from the MongoDB client!\n"
 
